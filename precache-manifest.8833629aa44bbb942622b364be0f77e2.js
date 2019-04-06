80df3867c956116d56cbadcd41b7220e10aa060b
Nuno Gervasio <nunogee@gmail.com>
2019-04-06 18:51:11 +0200

    Updates

---
+++ precache-manifest.8833629aa44bbb942622b364be0f77e2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e14c98d6ddab301cd8a6",
+    "url": "/profile/static/css/main.cf4e7b82.chunk.css"
+  },
+  {
+    "revision": "e14c98d6ddab301cd8a6",
+    "url": "/profile/static/js/main.1f974971.chunk.js"
+  },
+  {
+    "revision": "189fd871db148202566e",
+    "url": "/profile/static/js/runtime~main.cc8686f1.js"
+  },
+  {
+    "revision": "ee8030195a2f518134a6",
+    "url": "/profile/static/js/2.056dcdbc.chunk.js"
+  },
+  {
+    "revision": "91bc2833dc127abc72d74e991f80a58b",
+    "url": "/profile/index.html"
+  }
+];
